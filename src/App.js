996a111c652acd56c563904df99c6126e33999b5
sajadevo <nawabisajadahmad@gmail.com>
2021-04-14 02:05:42 -0700

    Update app page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import Sidebar from './components/Sidebar';
-import StatusCard from './components/StatusCard';
-import ChartLine from './components/ChartLine';
-import ChartBar from './components/ChartBar';
-import PageVisitsCard from './components/PageVisitsCard';
-import TrafficCard from './components/TrafficCard';
+import Dashboard from './pages/Dashboard';
 import '@material-tailwind/react/tailwind.css';
 
 function App() {
@@ -11,78 +7,65 @@ function App() {
         <>
             <Sidebar />
             <div className="md:ml-64">
-                <div className="bg-light-blue-500 pt-14 pb-28 px-9 h-auto">
-                    <div className="container mx-auto max-w-full">
-                        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4">
-                            <StatusCard
-                                color="red"
-                                icon="far fa-chart-bar"
-                                title="Traffic"
-                                amount="350,897"
-                                percentage="3.48"
-                                percentageIcon="fas fa-arrow-up"
-                                persentageColor="green"
-                                date="Since last month"
-                            />
-                            <StatusCard
-                                color="orange"
-                                icon="fas fa-chart-pie"
-                                title="New Users"
-                                amount="2,356"
-                                percentage="3.48"
-                                percentageIcon="fas fa-arrow-down"
-                                persentageColor="red"
-                                date="Since last week"
-                            />
-                            <StatusCard
-                                color="purple"
-                                icon="fas fa-users"
-                                title="Sales"
-                                amount="924"
-                                percentage="1.10"
-                                percentageIcon="fas fa-arrow-down"
-                                persentageColor="orange"
-                                date="Since yesterday"
-                            />
-                            <StatusCard
-                                color="teal"
-                                icon="fas fa-percentage"
-                                title="Performance"
-                                amount="49,65%"
-                                percentage="12"
-                                percentageIcon="fas fa-arrow-up"
-                                persentageColor="green"
-                                date="Since last month"
-                            />
-                        </div>
-                    </div>
-                </div>
+                <Dashboard />
 
-                <div className="px-9 h-auto -mt-24">
-                    <div className="container mx-auto max-w-full">
-                        <div className="grid grid-cols-1 xl:grid-cols-5">
-                            <div className="xl:col-start-1	xl:col-end-4 px-6">
-                                <ChartLine />
-                            </div>
-                            <div className="xl:col-start-4 xl:col-end-6 px-6">
-                                <ChartBar />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <footer className="py-6 px-16 border-t border-gray-200 font-light flex flex-col lg:flex-row justify-between items-center">
+                    <p className="text-gray-700 mb-6 lg:mb-0">
+                        Copyright &copy; {new Date().getFullYear()} Material
+                        Tailwind by{' '}
+                        <a
+                            href="https://www.creative-tim.com?ref=mtdk"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-light-blue-500 hover:text-light-blue-700"
+                        >
+                            Creative Tim
+                        </a>
+                    </p>
 
-                <div className="pb-14 px-9 h-auto">
-                    <div className="container mx-auto max-w-full">
-                        <div className="grid grid-cols-1 xl:grid-cols-5">
-                            <div className="xl:col-start-1	xl:col-end-4 px-6">
-                                <PageVisitsCard />
-                            </div>
-                            <div className="xl:col-start-4 xl:col-end-6 px-6">
-                                <TrafficCard />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    <ul className="list-unstyled flex">
+                        <li className="mr-6">
+                            <a
+                                className="text-gray-700 hover:text-gray-900 font-medium block pb-2 text-sm"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://github.com/creativetimofficial/material-tailwind/blob/main/LICENSE.md?ref=mtdk"
+                            >
+                                MIT License
+                            </a>
+                        </li>
+                        <li className="mr-6">
+                            <a
+                                className="text-gray-700 hover:text-gray-900 font-medium block pb-2 text-sm"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://github.com/creativetimofficial/material-tailwind/blob/main/CONTRIBUTING.md?ref=mtdk"
+                            >
+                                Contribute
+                            </a>
+                        </li>
+                        <li className="mr-6">
+                            <a
+                                className="text-gray-700 hover:text-gray-900 font-medium block pb-2 text-sm"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://github.com/creativetimofficial/material-tailwind/blob/main/CODE_OF_CONDUCT.md?ref=mtdk"
+                            >
+                                Code of Conduct
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="text-gray-700 hover:text-gray-900 font-medium block pb-2 text-sm"
+                                target="_blank"
+                                rel="noreferrer"
+                                href="https://creative-tim.com/contact-us?ref=mtdk"
+                            >
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+                </footer>
             </div>
         </>
     );
