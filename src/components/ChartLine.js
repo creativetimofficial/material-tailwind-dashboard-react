ed420bf0c0caabd0d40179cc86d2640c30dfe941
sajadevo <nawabisajadahmad@gmail.com>
2021-04-14 00:52:53 -0700

    Add chart line component

---
+++ src/components/ChartLine.js
@@ -0,0 +1,131 @@
+import { useEffect } from 'react';
+import Chart from 'chart.js';
+
+export default function ChartLine() {
+    useEffect(() => {
+        var config = {
+            type: 'line',
+            data: {
+                labels: [
+                    'January',
+                    'February',
+                    'March',
+                    'April',
+                    'May',
+                    'June',
+                    'July',
+                ],
+                datasets: [
+                    {
+                        label: new Date().getFullYear(),
+                        backgroundColor: '#03a9f4',
+                        borderColor: '#03a9f4',
+                        data: [65, 78, 66, 44, 56, 67, 75],
+                        fill: false,
+                    },
+                    {
+                        label: new Date().getFullYear() - 1,
+                        fill: false,
+                        backgroundColor: '#ff9800',
+                        borderColor: '#ff9800',
+                        data: [40, 68, 86, 74, 56, 60, 87],
+                    },
+                ],
+            },
+            options: {
+                maintainAspectRatio: false,
+                responsive: true,
+                title: {
+                    display: false,
+                    text: 'Sales Charts',
+                    fontColor: 'white',
+                },
+                legend: {
+                    labels: {
+                        fontColor: 'black',
+                    },
+                    align: 'end',
+                    position: 'bottom',
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false,
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true,
+                },
+                scales: {
+                    xAxes: [
+                        {
+                            ticks: {
+                                fontColor: 'rgba(17,17,17,.7)',
+                            },
+                            display: true,
+                            scaleLabel: {
+                                display: false,
+                                labelString: 'Month',
+                                fontColor: 'white',
+                            },
+                            gridLines: {
+                                display: false,
+                                borderDash: [2],
+                                borderDashOffset: [2],
+                                color: 'rgba(33, 37, 41, 0.3)',
+                                zeroLineColor: 'rgba(0, 0, 0, 0)',
+                                zeroLineBorderDash: [2],
+                                zeroLineBorderDashOffset: [2],
+                            },
+                        },
+                    ],
+                    yAxes: [
+                        {
+                            ticks: {
+                                fontColor: 'rgba(17,17,17,.7)',
+                            },
+                            display: true,
+                            scaleLabel: {
+                                display: false,
+                                labelString: 'Value',
+                                fontColor: 'white',
+                            },
+                            gridLines: {
+                                borderDash: [3],
+                                borderDashOffset: [3],
+                                drawBorder: false,
+                                color: 'rgba(17, 17, 17, 0.15)',
+                                zeroLineColor: 'rgba(33, 37, 41, 0)',
+                                zeroLineBorderDash: [2],
+                                zeroLineBorderDashOffset: [2],
+                            },
+                        },
+                    ],
+                },
+            },
+        };
+        var ctx = document.getElementById('line-chart').getContext('2d');
+        window.myLine = new Chart(ctx, config);
+    }, []);
+
+    return (
+        <>
+            <div className="relative flex flex-col min-w-0 break-words w-full mb-20 shadow rounded-lg bg-white px-4">
+                <div className="rounded-lg mb-0 px-6 py-4 bg-gradient-to-tr from-orange-500 to-orange-700 shadow-xl -mt-8">
+                    <div className="flex flex-wrap items-center">
+                        <div className="relative w-full max-w-full flex-grow flex-1">
+                            <h6 className="uppercase text-gray-200 mb-1 text-xs font-medium">
+                                Overview
+                            </h6>
+                            <h2 className="text-white text-2xl">Sales value</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-4 flex-auto">
+                    <div className="relative h-96">
+                        <canvas id="line-chart"></canvas>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
