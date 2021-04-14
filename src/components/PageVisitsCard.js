dc70bf7d265ffeb3782453bcd62a13d94e3949d1
sajadevo <nawabisajadahmad@gmail.com>
2021-04-14 01:00:45 -0700

    Add page visits card component

---
+++ src/components/PageVisitsCard.js
@@ -0,0 +1,100 @@
+import Button from '@material-tailwind/react/Button';
+
+export default function PageVisitsCard() {
+    return (
+        <>
+            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-20 shadow rounded-lg px-4">
+                <div className="rounded-lg mb-0 px-6 py-4 bg-gradient-to-tr from-teal-500 to-teal-700 shadow-xl -mt-8">
+                    <div className="flex flex-wrap items-center justify-between">
+                        <div className="relative max-w-full">
+                            <h3 className="text-xl text-white">Page visits</h3>
+                        </div>
+                        <div className="relative text-right">
+                            <Button color="lightBlue" size="sm" ripple="light">
+                                See More
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+                <div className="block w-full overflow-x-auto pt-6 pb-4 px-2">
+                    <table className="items-center w-full bg-transparent border-collapse">
+                        <thead>
+                            <tr>
+                                <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
+                                    ID
+                                </th>
+                                <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
+                                    Name
+                                </th>
+                                <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
+                                    Salary
+                                </th>
+                                <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
+                                    Country
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    1
+                                </th>
+                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    Dakota Rice
+                                </td>
+                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    $36,738
+                                </td>
+                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    Niger
+                                </td>
+                            </tr>
+                            <tr>
+                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    2
+                                </th>
+                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    Minerva Hooper
+                                </td>
+                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    $23,789
+                                </td>
+                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    Curaçao
+                                </td>
+                            </tr>
+                            <tr>
+                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    3
+                                </th>
+                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    Sage Rodriguez
+                                </td>
+                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    $56,142
+                                </td>
+                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    Netherlands
+                                </td>
+                            </tr>
+                            <tr>
+                                <th className="align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    4
+                                </th>
+                                <td className="align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    Philip Chaney
+                                </td>
+                                <td className="align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    $38,735
+                                </td>
+                                <td className="align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                    Korea, South
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
+    );
+}
