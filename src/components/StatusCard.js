c4c408d1e1fb37a050236435fa2213ad98be326f
sajadevo <nawabisajadahmad@gmail.com>
2021-05-05 05:59:12 -0700

    Convert the status card element to material tailwind card components

--- src/components/StatusCard.js
+++ src/components/StatusCard.js
@@ -1,3 +1,8 @@
+import Card from '@material-tailwind/react/Card';
+import CardRow from '@material-tailwind/react/CardRow';
+import CardHeader from '@material-tailwind/react/CardHeader';
+import CardStatus from '@material-tailwind/react/CardStatus';
+import CardStatusFooter from '@material-tailwind/react/CardStatusFooter';
 import Icon from '@material-tailwind/react/Icon';
 
 export default function StatusCard({
@@ -6,51 +11,38 @@ export default function StatusCard({
     title,
     amount,
     percentage,
+    percentageColor,
     percentageIcon,
-    persentageColor,
     date,
 }) {
     return (
-        <div className="w-full px-5 mb-10">
-            <div className="relative flex flex-col min-w-0 break-words bg-white rounded-lg mb-6 xl:mb-0 shadow">
-                <div className="flex-auto p-4">
-                    <div className="flex flex-wrap border-b border-gray-200 pb-4">
-                        <div className="relative w-auto -mt-10 flex-initial h-full">
-                            <div
-                                className={`h-24 w-24 p-3 text-center grid items-center shadow-xl rounded-lg bg-gradient-to-tr from-${color}-500 to-${color}-700`}
-                            >
-                                <Icon
-                                    family="font-awesome"
-                                    name={icon}
-                                    size="3xl"
-                                    color="white"
-                                />
-                            </div>
-                        </div>
+        <div className="px-4 mb-10">
+            <Card>
+                <CardRow>
+                    <CardHeader color={color} size="lg" iconOnly>
+                        <Icon
+                            family="font-awesome"
+                            name={icon}
+                            size="3xl"
+                            color="white"
+                        />
+                    </CardHeader>
 
-                        <div className="relative w-full pl-4 max-w-full flex-grow flex-1 mb-2 text-right">
-                            <h5 className="text-gray-500 font-light tracking-wide text-sm mb-1">
-                                {title}
-                            </h5>
-                            <span className="text-2xl text-gray-900">
-                                {amount}
-                            </span>
-                        </div>
-                    </div>
+                    <CardStatus title={title} amount={amount} />
+                </CardRow>
 
-                    <p className="text-sm text-gray-700 pt-4">
-                        <span
-                            className={`text-${persentageColor}-500 font-light mr-2`}
-                        >
-                            <Icon family="font-awesome" name={percentageIcon} />
-                            &nbsp;{percentage}%
-                        </span>
-                        <span className="font-light whitespace-nowrap">
-                            {date}
-                        </span>
-                    </p>
-                </div>
-            </div>
+                <CardStatusFooter
+                    amount={percentage}
+                    color={percentageColor}
+                    date={date}
+                >
+                    <Icon
+                        family="font-awesome"
+                        color={percentageColor}
+                        name={percentageIcon}
+                    />
+                </CardStatusFooter>
+            </Card>
         </div>
     );
 }
