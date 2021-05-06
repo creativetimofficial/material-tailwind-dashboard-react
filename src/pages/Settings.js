0d4a43bd1f97b8901c97caa2b2aff1c526417b11
sajadevo <nawabisajadahmad@gmail.com>
2021-05-06 03:36:35 -0700

    Update the setting form component

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -52,13 +52,13 @@ export default function Dashboard() {
                 </div>
             </div>
 
-            <div className="px-3 md:px-8 h-auto -mt-20">
+            <div className="px-3 md:px-8 h-auto -mt-24">
                 <div className="container mx-auto max-w-full">
                     <div className="grid grid-cols-1 xl:grid-cols-6">
-                        <div className="xl:col-start-1 xl:col-end-5 px-4">
+                        <div className="xl:col-start-1 xl:col-end-5 px-4 mb-16">
                             <SettingsForm />
                         </div>
-                        <div className="xl:col-start-5 xl:col-end-7 px-4">
+                        <div className="xl:col-start-5 xl:col-end-7 px-4 mb-16">
                             <ProfileCard />
                         </div>
                     </div>
