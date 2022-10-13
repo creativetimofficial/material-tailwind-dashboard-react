d6523d743fe43a925ffe0cdf032f1989bfb480e4
sajadevo <nawabisajadahmad@gmail.com>
2022-10-13 14:54:08 +0430

    add routes

---
+++ src/routes.jsx
@@ -0,0 +1,56 @@
+import {
+  HomeIcon,
+  UserCircleIcon,
+  TableCellsIcon,
+  MapIcon,
+  ArrowRightOnRectangleIcon,
+  UserPlusIcon,
+} from "@heroicons/react/24/solid";
+import { Home } from "@/pages/dashboard";
+
+const icon = {
+  className: "w-5 h-5 text-inherit",
+};
+
+export const routes = [
+  {
+    layout: "dashboard",
+    pages: [
+      {
+        icon: <HomeIcon {...icon} />,
+        name: "dashboard",
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        icon: <UserCircleIcon {...icon} />,
+        name: "profile",
+        path: "/profile",
+      },
+      {
+        icon: <TableCellsIcon {...icon} />,
+        name: "tables",
+        path: "/tables",
+      },
+      { icon: <MapIcon {...icon} />, name: "maps", path: "/maps" },
+    ],
+  },
+  {
+    title: "auth pages",
+    layout: "auth",
+    pages: [
+      {
+        icon: <ArrowRightOnRectangleIcon {...icon} />,
+        name: "sign in",
+        path: "/auth/sign-in",
+      },
+      {
+        icon: <UserPlusIcon {...icon} />,
+        name: "sign up",
+        path: "/auth/sign-up",
+      },
+    ],
+  },
+];
+
+export default routes;
