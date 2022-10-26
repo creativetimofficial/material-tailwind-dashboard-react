c3fd85cb8e6a25685106c09f9a937929fd652040
sajadevo <nawabisajadahmad@gmail.com>
2022-10-26 12:04:07 +0430

    update home page

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -50,7 +50,7 @@ export function Home() {
           />
         ))}
       </div>
-      <div className="mb-6 grid gap-y-12 gap-x-6 md:grid-cols-2 xl:grid-cols-3">
+      <div className="mb-6 grid grid-cols-1 gap-y-12 gap-x-6 md:grid-cols-2 xl:grid-cols-3">
         {statisticsChartsData.map((props) => (
           <StatisticsChart
             key={props.title}
