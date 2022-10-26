a4b59b946836eb25b4dbc9802a9a07bccd86d415
sajadevo <nawabisajadahmad@gmail.com>
2022-10-26 12:03:56 +0430

    update dashboard-navbar

--- src/widgets/layout/dashboard-navbar.jsx
+++ src/widgets/layout/dashboard-navbar.jsx
@@ -18,12 +18,17 @@ import {
   BellIcon,
   ClockIcon,
   CreditCardIcon,
+  Bars3Icon,
 } from "@heroicons/react/24/solid";
-import { useMaterialTailwindController, setOpenConfigurator } from "@/context";
+import {
+  useMaterialTailwindController,
+  setOpenConfigurator,
+  setOpenSidenav,
+} from "@/context";
 
 export function DashboardNavbar() {
   const [controller, dispatch] = useMaterialTailwindController();
-  const { fixedNavbar } = controller;
+  const { fixedNavbar, openSidenav } = controller;
   const { pathname } = useLocation();
   const [layout, page] = pathname.split("/").filter((el) => el !== "");
 
@@ -32,13 +37,13 @@ export function DashboardNavbar() {
       color={fixedNavbar ? "white" : "transparent"}
       className={`rounded-xl transition-all ${
         fixedNavbar
-          ? "sticky top-4 py-3 shadow-md shadow-blue-gray-500/5"
+          ? "sticky top-4 z-40 py-3 shadow-md shadow-blue-gray-500/5"
           : "px-0 py-1"
       }`}
       fullWidth
       blurred={fixedNavbar}
     >
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col-reverse justify-between gap-6 md:flex-row md:items-center">
         <div className="capitalize">
           <Breadcrumbs
             className={`bg-transparent p-0 transition-all ${
@@ -67,18 +72,33 @@ export function DashboardNavbar() {
           </Typography>
         </div>
         <div className="flex items-center">
-          <div className="mr-4 w-56">
+          <div className="mr-auto md:mr-4 md:w-56">
             <Input label="Type here" />
           </div>
+          <IconButton
+            variant="text"
+            color="blue-gray"
+            className="grid xl:hidden"
+            onClick={() => setOpenSidenav(dispatch, !openSidenav)}
+          >
+            <Bars3Icon strokeWidth={3} className="h-6 w-6 text-blue-gray-500" />
+          </IconButton>
           <Link to="/auth/sign-in">
             <Button
               variant="text"
               color="blue-gray"
-              className="flex items-center gap-1 px-4"
+              className="hidden items-center gap-1 px-4 xl:flex"
             >
               <UserCircleIcon className="h-5 w-5 text-blue-gray-500" />
               Sign In
             </Button>
+            <IconButton
+              variant="text"
+              color="blue-gray"
+              className="grid xl:hidden"
+            >
+              <UserCircleIcon className="h-5 w-5 text-blue-gray-500" />
+            </IconButton>
           </Link>
           <IconButton
             variant="text"
