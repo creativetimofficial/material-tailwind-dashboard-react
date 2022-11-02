d0f0bdd8f37e297df19744eb4af9fe48b9850ba4
sajadevo <nawabisajadahmad@gmail.com>
2022-11-02 11:45:10 +0430

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/material-tailwind-dashboard-react/",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
