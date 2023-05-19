31541ac1359216794ea44713786df36d9ccedda7
muzammalrahim <creative.joomdev@gmail.com>
2023-05-19 15:25:31 +0500

    ABuBAkar Branch Working

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard, Auth } from "@/layouts";
 
 function App() {
-  console.log("App");
+  console.log("AbuBAkar");
   return (
     <Routes>
       <Route path="/dashboard/*" element={<Dashboard />} />
