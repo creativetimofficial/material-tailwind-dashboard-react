e1279f4d34ed7bd36ccbe865c61e0c3e9d6b1d8a
muzammalrahim <creative.joomdev@gmail.com>
2023-05-22 02:50:12 +0500

    API BASE URL CHANGE

--- src/apiConfig/axiosInstance.js
+++ src/apiConfig/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:1000",
+  baseURL: "https://d4f0-206-84-142-90.ngrok-free.app",
+  // baseURL: "http://localhost:1000",
 });
 
 // Set the user token if available
