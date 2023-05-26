238d9d6a2436446f966280b3c4180c14f7e85ff4
muzammalrahim <creative.joomdev@gmail.com>
2023-05-26 11:34:26 +0500

    Get Employees and View Done

--- src/pages/dashboard/employee.jsx
+++ src/pages/dashboard/employee.jsx
@@ -1,20 +1,23 @@
+import ViewEmployee from "@/widgets/employee/viewEmployee";
 import { Button } from "@material-tailwind/react";
 import { set } from "date-fns";
 import React, { useState } from "react";
 
 export const Employee = () => {
-  const [show, setshow] = useState(false);
+  const [show, setshow] = useState(true);
 
   return (
     <>
       <div>
         {/* Buttons */}
-        <div className="mt-3 flex items-center justify-center gap-x-6">
+        <div className="mt-10 flex items-center justify-center gap-x-6">
           <Button onClick={() => setshow(true)}>View Employees</Button>
           <Button onClick={() => setshow(false)}>Add Employees</Button>
         </div>
-        <div className="mt-32 flex items-center justify-center text-2xl">
-          {show && <div className="">View Eplmoyess Component</div>}
+        <div className="mt-10 flex items-center justify-center ">
+          {show && <div className="">
+            <ViewEmployee/>
+            </div>}
           {!show && <div className="">Add Eplmoyess Component</div>}
         </div>
       </div>
