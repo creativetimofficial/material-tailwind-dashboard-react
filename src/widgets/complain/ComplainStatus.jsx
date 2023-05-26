0872ea881f8d23608a3d4ab845f304698de91067
muzammalrahim <creative.joomdev@gmail.com>
2023-05-26 18:53:35 +0500

    Status Color Change

--- src/widgets/complain/ComplainStatus.jsx
+++ src/widgets/complain/ComplainStatus.jsx
@@ -4,7 +4,8 @@ const ComplainStatus = ({ name }) => {
   const colorr =
     name === "Pending" ? "#ae1b1b" : name === "In-Progress" && "#3890e3";
   return (
-    <span className={`rounded-xl bg-[${colorr}] px-4  py-1.5 text-white`}>
+    // <span className={`rounded-xl bg-[${"black"}] px-4  py-1.5 text-white`}>
+    <span className={`rounded-xl bg-black px-4  py-1.5 text-white`}>
       {name}
     </span>
   );
