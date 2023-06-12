bf25dc741a126b16867951527fba0d12734c75cd
muzammalrahim <creative.joomdev@gmail.com>
2023-06-12 12:20:35 +0500

    User Suggestions

--- src/widgets/complain/ComplainSearch.jsx
+++ src/widgets/complain/ComplainSearch.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import SelectStatus from "../htmlComponents/StatusSelect";
 import { Button, Input } from "@material-tailwind/react";
 import DateSelector from "../htmlComponents/DateSelector";
+import SearchInput from "../htmlComponents/SearchInput";
 
 const ComplainSearch = ({ fetchComplains }) => {
   const [searchData, setsearchData] = useState({});
+  console.log(searchData, "From Parent");
   const [user, setuser] = useState("");
   let check;
   const checkOnChange = (e) => {
@@ -17,6 +19,7 @@ const ComplainSearch = ({ fetchComplains }) => {
   };
   const handleSearch = () => {
     console.log({ ...searchData, user: check });
+
     fetchComplains({ ...searchData, user: check });
   };
   return (
@@ -24,13 +27,18 @@ const ComplainSearch = ({ fetchComplains }) => {
       <div className="flex  gap-6">
         <SelectStatus setSearch={setsearchData} />
         <DateSelector setSearch={setsearchData} />
+        {1 === 2 && (
+          <div>
+            <Input
+              className="w-62"
+              label="Search Number or Name"
+              type="text"
+              onChange={(e) => checkOnChange(e.target.value)}
+            />
+          </div>
+        )}
         <div>
-          <Input
-            className="w-62"
-            label="Search Number or Name"
-            type="text"
-            onChange={(e) => checkOnChange(e.target.value)}
-          />
+          <SearchInput />
         </div>
         <Button onClick={handleSearch}>Search Complaints</Button>
       </div>
