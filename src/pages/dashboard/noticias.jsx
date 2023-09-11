9bda77f15fd6a7d91458fa7b5c89613ac8152c1a
Aquileo León <leon.automatizacion@gmail.com>
2023-09-11 13:23:17 -0600

    Se elimina Statistics-Chart del proyecto y sus implementaciones

--- src/pages/dashboard/noticias.jsx
+++ src/pages/dashboard/noticias.jsx
@@ -20,7 +20,6 @@ import {
   ArrowUpIcon,
 } from "@heroicons/react/24/outline";
 import { StatisticsCard } from "@/widgets/cards";
-import { StatisticsChart } from "@/widgets/charts";
 import {
   statisticsCardsData,
   statisticsChartsData,
