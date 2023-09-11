79c91a6f93e69d74394dfade0b0bde9e8cada378
Aquileo León <leon.automatizacion@gmail.com>
2023-09-11 16:11:20 -0600

    Se elimina importación de Footer

--- src/layouts/auth.jsx
+++ src/layouts/auth.jsx
@@ -5,7 +5,7 @@ import {
   UserPlusIcon,
   ArrowRightOnRectangleIcon,
 } from "@heroicons/react/24/solid";
-import { Navbar, Footer } from "@/widgets/layout";
+import { Navbar } from "@/widgets/layout";
 import routes from "@/routes";
 
 export function Auth() {
