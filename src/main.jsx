b850f7b84067a82ca427d8ad10cbbfde664264d4
Aquileo León <leon.automatizacion@gmail.com>
2023-09-11 12:15:26 -0600

    Se modifica Metadatos del proyecto en Title

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,3 @@
-/**
-=========================================================
-* Material Tailwind Dashboard React - v2.0.0
-=========================================================
-* Product Page: https://www.creative-tim.com/product/material-tailwind-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/material-tailwind-dashboard-react/blob/main/LICENSE.md)
-* Coded by Creative Tim
-=========================================================
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
