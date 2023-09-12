1bdd64103a885607532e532d2a799376ecee28f9
Aquileo León <leon.automatizacion@gmail.com>
2023-09-12 13:36:57 -0600

    Se elimino código inutilizado de Auth

--- src/layouts/auth.jsx
+++ src/layouts/auth.jsx
@@ -1,50 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import {
-  ChartPieIcon,
-  UserIcon,
-  UserPlusIcon,
-  ArrowRightOnRectangleIcon,
-} from "@heroicons/react/24/solid";
-import { Navbar } from "@/widgets/layout";
-import routes from "@/routes";
+
 
 export function Auth() {
-  const navbarRoutes = [
-    {
-      name: "gestor",
-      path: "/gestor/noticias",
-      icon: ChartPieIcon,
-    },
-    {
-      name: "sign up",
-      path: "/auth/sign-up",
-      icon: UserPlusIcon,
-    },
-    {
-      name: "sign in",
-      path: "/auth/sign-in",
-      icon: ArrowRightOnRectangleIcon,
-    },
-  ];
-
   return (
-    <div className="relative min-h-screen w-full">
-      <div className="container relative z-40 mx-auto p-4">
-        <Navbar routes={navbarRoutes} />
-      </div>
-      <Routes>
-        {routes.map(
-          ({ layout, pages }) =>
-            layout === "auth" &&
-            pages.map(({ path, element }) => (
-              <Route exact path={path} element={element} />
-            ))
-        )}
-      </Routes>
-      <div className="container absolute bottom-8 left-2/4 z-10 mx-auto -translate-x-2/4 text-white">
-        <Footer />
-      </div>
-    </div>
+    <h1>Login</h1>
   );
 }
 
