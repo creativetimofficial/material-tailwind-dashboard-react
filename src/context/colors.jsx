42a2af8634a41b0b115657d8f51b794d3de80d38
Aquileo León <leon.automatizacion@gmail.com>
2023-09-13 11:32:46 -0600

    Se corrigen bugs

---
+++ src/context/colors.jsx
@@ -0,0 +1,4 @@
+export default {    
+  primary : "red",
+  secondary : "green",
+  }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,12 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_sidebar : 'linear-gradient(to right, #ff0000, #808080)',
+        secondary_sidebar : 'linear-gradient(to right, #00ff00, #808080)'        
+      },
+    },
   },
   plugins: [],
 });
