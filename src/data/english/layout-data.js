57fb0ff22f2a72570638e3899ed7dc11e94a5efb
reza zeraatkar <rezak@gmail.com>
2023-12-11 14:33:04 +0330

    structure of layoutData changed

--- src/data/english/layout-data.js
+++ src/data/english/layout-data.js
@@ -1,15 +1,43 @@
+import { ChatBubbleLeftEllipsisIcon, Cog6ToothIcon, HomeIcon } from "@heroicons/react/24/solid";
+
 export const layoutData = {
-  dashboradConfiguratorTitle: "Dashboard Configurator",
-  dashboradConfiguratorSubtitle: "See our dashboard options.",
-  dashboradConfiguratorSelectLanguage: "Select Language",
-  dashboradSidenavColorsTitle: "Sidenav Colors",
-  dashboradSidenavTypesTitle: "Sidenav Types",
-  dashboradSidenavTypesSubtitle: "Choose between 3 different sidenav types.",
-  dashboradSidenavNavbarType: "Navbar Fixed",
-  projectsHeader: "Projects",
-  projectsSubHeader: "30 done this month.",
-  projectsMenuItems: ["Action", "Another action", "something else here"],
-  projectsTableHeaders: ["companies", "members", "budget", "completion"],
-  ordersOverviewHeader: "Orders Overview",
-  ordersOverviewSubHeader: "This month 24%",
+  homePage: {
+    dashboradConfiguratorTitle: "Dashboard Configurator",
+    dashboradConfiguratorSubtitle: "See our dashboard options.",
+    dashboradConfiguratorSelectLanguage: "Select Language",
+    dashboradSidenavColorsTitle: "Sidenav Colors",
+    dashboradSidenavTypesTitle: "Sidenav Types",
+    dashboradSidenavTypesSubtitle: "Choose between 3 different sidenav types.",
+    dashboradSidenavNavbarType: "Navbar Fixed",
+    projectsHeader: "Projects",
+    projectsSubHeader: "30 done this month.",
+    projectsMenuItems: ["Action", "Another action", "something else here"],
+    projectsTableHeaders: ["companies", "members", "budget", "completion"],
+    ordersOverviewHeader: "Orders Overview",
+    ordersOverviewSubHeader: "This month 24%",
+  },
+  profilePage: {
+    profileName: "",
+    profileSkill: "",
+    tabs:[
+      { 
+        value: "app", 
+        icon: HomeIcon, 
+        label: "App", 
+        className: "-mt-1 me-2 inline-block h-5 w-5" 
+      },
+      { 
+        value: "message", 
+        icon: ChatBubbleLeftEllipsisIcon, 
+        label: "Message", 
+        className: "-mt-0.5 me-2 inline-block h-5 w-5" 
+      },
+      { 
+        value: "settings", 
+        icon: Cog6ToothIcon, 
+        label: "Settings", 
+        className: "-mt-1 me-2 inline-block h-5 w-5" 
+      },
+    ]
+  }
 }
--- src/data/persian/layout-data.js
+++ src/data/persian/layout-data.js
@@ -1,15 +1,43 @@
+import { ChatBubbleLeftEllipsisIcon, Cog6ToothIcon, HomeIcon } from "@heroicons/react/24/solid";
+
 export const layoutData = {
-  dashboradConfiguratorTitle: "تنظیمات داشبورد",
-  dashboradConfiguratorSubtitle: "گزینه‌های داشبورد ما را ببینید.",
-  dashboradConfiguratorSelectLanguage: "انتخاب زبان",
-  dashboradSidenavColorsTitle: "رنگ‌های نوارکناری",
-  dashboradSidenavTypesTitle: "انواع نوارکناری",
-  dashboradSidenavTypesSubtitle: "یکی از سه نوع نوارکناری را انتخاب کنید.",
-  dashboradSidenavNavbarType: "هدر ثابت",
-  projectsHeader: "پروژه‌ها",
-  projectsSubHeader: "این ماه 30 پروژه انجام شده است.",
-  projectsMenuItems: ["اقدام", "اقدامی دیگر", "یک اقدام دیگر"],
-  projectsTableHeaders: ["شرکتها", "اعضا", "بودجه", "تکمیل"],
-  ordersOverviewHeader: "نگاهی بر سفارش‌ها",
-  ordersOverviewSubHeader:  "این ماه 24 درصد",
+  homePage: {
+    dashboradConfiguratorTitle: "تنظیمات داشبورد",
+    dashboradConfiguratorSubtitle: "گزینه‌های داشبورد ما را ببینید.",
+    dashboradConfiguratorSelectLanguage: "انتخاب زبان",
+    dashboradSidenavColorsTitle: "رنگ‌های نوارکناری",
+    dashboradSidenavTypesTitle: "انواع نوارکناری",
+    dashboradSidenavTypesSubtitle: "یکی از سه نوع نوارکناری را انتخاب کنید.",
+    dashboradSidenavNavbarType: "هدر ثابت",
+    projectsHeader: "پروژه‌ها",
+    projectsSubHeader: "این ماه 30 پروژه انجام شده است.",
+    projectsMenuItems: ["اقدام", "اقدامی دیگر", "یک اقدام دیگر"],
+    projectsTableHeaders: ["شرکتها", "اعضا", "بودجه", "تکمیل"],
+    ordersOverviewHeader: "نگاهی بر سفارش‌ها",
+    ordersOverviewSubHeader:  "این ماه 24 درصد",
+  },
+  profilePage: {
+    profileName: "",
+    profileSkill: "",
+    tabs:[
+      { 
+        value: "app", 
+        icon: HomeIcon, 
+        label: "اپ", 
+        className: "-mt-1 me-2 inline-block h-5 w-5" 
+      },
+      { 
+        value: "message", 
+        icon: ChatBubbleLeftEllipsisIcon, 
+        label: "پیام", 
+        className: "-mt-0.5 me-2 inline-block h-5 w-5" 
+      },
+      { 
+        value: "settings", 
+        icon: Cog6ToothIcon, 
+        label: "تنظیمات",
+        className: "-mt-1 me-2 inline-block h-5 w-5" 
+      },
+    ]
+  }
 }
