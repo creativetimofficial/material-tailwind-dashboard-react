d340d4ec4329077bf695b8bc80f74a88f3ee56f2
Khatai Huseynzada <huseynzadekhatai@gmail.com>
2026-01-07 18:48:44 +0400

    some corrections

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCuO7kUgfPk5w0sCsJt9WPlWfint2IRNO0',
-  authDomain: 'admin-dashboard-d8efe.firebaseapp.com',
-  projectId: 'admin-dashboard-d8efe',
-  storageBucket: 'admin-dashboard-d8efe.firebasestorage.app',
-  messagingSenderId: '573808900654',
-  appId: '1:573808900654:web:d9692e7f9956c2e81dbdbf',
-  measurementId: 'G-R17ZJBK2J2',
+  apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+  authDomain: 'XXXXXXXXXXXXXX.firebaseapp.com',
+  projectId: 'XXXXXXXXXXXXXX',
+  storageBucket: 'XXXXXXXXXXXXXX.firebasestorage.app',
+  messagingSenderId: 'XXXXXXXXXXXXXXXXX',
+  appId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+  measurementId: 'XXXXXXXXXXXXXXXXXXXXXX',
 };
 
 // Initialize Firebase
